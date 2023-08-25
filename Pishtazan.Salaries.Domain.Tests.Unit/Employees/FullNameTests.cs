7f7e4c04c220f7dbfbe175b00eef7a50820c2508
m <m@m.com>
2023-08-25 14:42:54 +0330

    Grouping related first and last names as employee search is base on fullName

---
+++ Pishtazan.Salaries.Domain.Tests.Unit/Employees/FullNameTests.cs
@@ -0,0 +1,25 @@
+﻿using Pishtazan.Salaries.Domain.Common.Salaries;
+using Pishtazan.Salaries.Domain.Employees;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Pishtazan.Salaries.Domain.Tests.Unit.Employees
+{
+    public class FullNameTests
+    {
+        [Fact]
+        public void Constructor_NullFirstName_ThrowsArgumentNullException()
+        {
+            Assert.Throws<ArgumentNullException>(() => new FullName(firstName: null!, new LastName("ahmadi")));
+        }
+
+        [Fact]
+        public void Constructor_NullLastName_ThrowsArgumentNullException()
+        {
+            Assert.Throws<ArgumentNullException>(() => new FullName(new FirstName("ali"), lastName: null!));
+        }
+    }
+}
---
+++ Pishtazan.Salaries.Domain/Employees/FullName.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using static Pishtazan.Salaries.Infrastructure.Validation.Validate;
+
+namespace Pishtazan.Salaries.Domain.Employees
+{
+    public class FullName
+    {
+        public FirstName FirstName { get; private set; }
+        public LastName LastName { get; private set; }
+
+        public FullName(FirstName firstName, LastName lastName)
+        {
+            FirstName = ArgumentNotNull(firstName, nameof(firstName));
+            LastName = ArgumentNotNull(lastName, nameof(lastName));
+        }
+    }
+}
