ad95f7934de6fdd9f63e704616b819f45609e8d1
m <m@m.com>
2023-08-25 12:00:04 +0330

    Add more extra tests for invalid date str

--- Pishtazan.Salaries.Domain.Tests.Unit/Employees/DateTests.cs
+++ Pishtazan.Salaries.Domain.Tests.Unit/Employees/DateTests.cs
@@ -25,6 +25,9 @@ namespace Pishtazan.Salaries.Domain.Tests.Unit.Employees
         [InlineData("1400010")]
         [InlineData("1400010 ")]
         [InlineData(" 1400010 ")]
+        [InlineData(" 1400010a")]
+        [InlineData("1400010a")]
+        [InlineData("a14000101")]
         public void FromString_IncompleteAndInvalidDateStr_ThrowsArgumentOutOfRangeException(string dateStr)
         {
             Assert.Throws<ArgumentOutOfRangeException>(() => Date.FromString(dateStr));
