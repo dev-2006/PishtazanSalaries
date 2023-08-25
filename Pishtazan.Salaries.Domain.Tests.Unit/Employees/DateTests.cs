8200127d780c557b2e4a8ac7efcc1753069087f9
m <m@m.com>
2023-08-25 11:48:53 +0330

    Adding support for salary date by adding Date class and failing test

---
+++ Pishtazan.Salaries.Domain.Tests.Unit/Employees/DateTests.cs
@@ -0,0 +1,18 @@
+﻿using Pishtazan.Salaries.Domain.Employees;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Pishtazan.Salaries.Domain.Tests.Unit.Employees
+{
+    public class DateTests
+    {
+        [Fact]
+        public void FromString_NullArgument_ThrowsArgumentNullException()
+        {
+            Assert.Throws<ArgumentNullException>(() => Date.FromString(null!));
+        }
+    }
+}
---
+++ Pishtazan.Salaries.Domain/Employees/Date.cs
@@ -0,0 +1,14 @@
+﻿using Pishtazan.Salaries.Domain.Common;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Pishtazan.Salaries.Domain.Employees
+{
+    public record Date
+    {
+        public static Date FromString(string value) => throw new NotImplementedException();
+    }
+}
