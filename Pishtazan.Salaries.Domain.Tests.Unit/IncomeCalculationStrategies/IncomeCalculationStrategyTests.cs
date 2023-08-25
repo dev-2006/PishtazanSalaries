d17ed088e735d73a6e6004c251cac973bc58ac83
m <m@m.com>
2023-08-25 13:50:53 +0330

    IncomeCalculationStrategy added for incom calculations

---
+++ Pishtazan.Salaries.Domain.Tests.Unit/IncomeCalculationStrategies/IncomeCalculationStrategyTests.cs
@@ -0,0 +1,29 @@
+﻿using Pishtazan.Salaries.Domain.Common.Salaries;
+using Pishtazan.Salaries.Domain.IncomeCalculationStrategies;
+using Pishtazan.Salaries.OvertimePolicies.Calculators;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Pishtazan.Salaries.Domain.Tests.Unit.IncomeCalculationStrategies
+{
+    public class IncomeCalculationStrategyTests
+    {
+        IncomeCalculationStrategy startegy = new IncomeCalculationStrategy();
+
+        [Fact]
+        public void Calculate_NullSalaryDetail_ThrowsArgumentNullException()
+        {
+            Assert.Throws<ArgumentNullException>(() => startegy.Calculate(null!, new CalculatorA()));
+        }
+
+        [Fact]
+        public void Calculate_NullOvertimeCalculator_ThrowsArgumentNullException()
+        {
+            Assert.Throws<ArgumentNullException>(() => startegy.Calculate(
+                new SalaryDetail(new BasicSalary(1), new Allowance(1), new Transportation(1)), null!));
+        }
+    }
+}
---
+++ Pishtazan.Salaries.Domain/IncomeCalculationStrategies/IncomeCalculationStrategy.cs
@@ -0,0 +1,15 @@
+﻿using Pishtazan.Salaries.Domain.Common.Salaries;
+using Pishtazan.Salaries.OvertimePolicies.Calculators;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Pishtazan.Salaries.Domain.IncomeCalculationStrategies
+{
+    public class IncomeCalculationStrategy
+    {
+        public Income Calculate(SalaryDetail salaryDetail, IOvertimePolicyCalculator overtimeCalculator) => throw new NotImplementedException();
+    }
+}
