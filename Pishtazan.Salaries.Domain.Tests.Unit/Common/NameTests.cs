51e5f1dcc296915ae5dac7c3386b9d9bdc24f589
m <m@m.com>
2023-08-25 06:45:20 +0330

    Name value object and failing test for null argument in ctor added

---
+++ Pishtazan.Salaries.Domain.Tests.Unit/Common/NameTests.cs
@@ -0,0 +1,20 @@
+﻿using Pishtazan.Salaries.Domain.Common;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Pishtazan.Salaries.Domain.Tests.Unit.Common
+{
+    public class NameTests
+    {
+        [Fact]
+        public void Constructor_NullArgument_ThrowsArgumentNullException()
+        {
+            var e = Record.Exception(() => new Name(null!));
+
+            Assert.IsType<ArgumentNullException>(e);
+        }
+    }
+}
---
+++ Pishtazan.Salaries.Domain/Common/Name.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Pishtazan.Salaries.Domain.Common
+{
+    public record Name
+    {
+        public Name(string value)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
