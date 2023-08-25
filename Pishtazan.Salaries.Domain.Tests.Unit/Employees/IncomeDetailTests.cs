ff6eb4d3e0f05012e382fca5313a6c29cf99c154
m <m@m.com>
2023-08-25 14:36:51 +0330

    Grouping income related data into incomeDetail as each employee has many incomeDetails

---
+++ Pishtazan.Salaries.Domain.Tests.Unit/Employees/IncomeDetailTests.cs
@@ -0,0 +1,35 @@
+﻿using Pishtazan.Salaries.Domain.Common.Salaries;
+using Pishtazan.Salaries.Domain.Employees;
+using Pishtazan.Salaries.OvertimePolicies.Calculators;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Pishtazan.Salaries.Domain.Tests.Unit.Employees
+{
+    public class IncomeDetailTests
+    {
+        [Fact]
+        public void Constructor_NullDate_ThrowsArgumentNullException()
+        {
+            Assert.Throws<ArgumentNullException>(() => new IncomeDetail(date: null!, 
+                new SalaryDetail(new BasicSalary(1), new Allowance(1), new Transportation(1)), new Income(1)));
+        }
+
+        [Fact]
+        public void Constructor_NullSalaryDetail_ThrowsArgumentNullException()
+        {
+            Assert.Throws<ArgumentNullException>(() => new IncomeDetail(Date.FromString("14000101"),
+               salaryDetails: null!, new Income(1)));
+        }
+
+        [Fact]
+        public void Constructor_NullIncome_ThrowsArgumentNullException()
+        {
+            Assert.Throws<ArgumentNullException>(() => new IncomeDetail(Date.FromString("14000101"),
+                new SalaryDetail(new BasicSalary(1), new Allowance(1), new Transportation(1)), income: null!));
+        }
+    }
+}
---
+++ Pishtazan.Salaries.Domain/Employees/IncomeDetail.cs
@@ -0,0 +1,24 @@
+﻿using Pishtazan.Salaries.Domain.Common.Salaries;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using static Pishtazan.Salaries.Infrastructure.Validation.Validate;
+
+namespace Pishtazan.Salaries.Domain.Employees
+{
+    public class IncomeDetail
+    {
+        public Date Date { get; private set; }
+        public SalaryDetail SalaryDetails { get; private set; }
+        public Income Income { get; private set; }
+
+        public IncomeDetail(Date date, SalaryDetail salaryDetails, Income income)
+        {
+            Date = ArgumentNotNull(date, nameof(date));
+            SalaryDetails = ArgumentNotNull(salaryDetails, nameof(salaryDetails));
+            Income = ArgumentNotNull(income, nameof(income));
+        }
+    }
+}
