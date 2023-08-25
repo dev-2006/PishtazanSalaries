87d231f01870da99adb296951809c3555ba828c3
m <m@m.com>
2023-08-25 13:11:33 +0330

    Improve coding by grouping salary related classes into SalaryDetail class

---
+++ Pishtazan.Salaries.Domain.Tests.Unit/Employees/Salaries/SalaryDetailTests.cs
@@ -0,0 +1,30 @@
+﻿using Pishtazan.Salaries.Domain.Employees.Salaries;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Pishtazan.Salaries.Domain.Tests.Unit.Employees.Salaries
+{
+    public class SalaryDetailTests
+    {
+        [Fact]
+        public void Constructor_NullBasicSalary_ThrowsArgumentNullException()
+        {
+            Assert.Throws<ArgumentNullException>(() => new SalaryDetail(null!, new Allowance(1L), new Transportation(1L)));
+        }
+
+        [Fact]
+        public void Constructor_NullAllowance_ThrowsArgumentNullException()
+        {
+            Assert.Throws<ArgumentNullException>(() => new SalaryDetail(new BasicSalary(1L), null!, new Transportation(1L)));
+        }
+
+        [Fact]
+        public void Constructor_NullTransportation_ThrowsArgumentNullException()
+        {
+            Assert.Throws<ArgumentNullException>(() => new SalaryDetail(new BasicSalary(1L), new Allowance(1L), null!));
+        }
+    }
+}
---
+++ Pishtazan.Salaries.Domain/Employees/Salaries/SalaryDetail.cs
@@ -0,0 +1,23 @@
+﻿using Pishtazan.Salaries.Infrastructure.Validation;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Pishtazan.Salaries.Domain.Employees.Salaries
+{
+    public class SalaryDetail
+    {
+        public BasicSalary BasicSalary { get; private set; }
+        public Allowance Allowance { get; private set; }
+        public Transportation Transportation { get; private set; }
+
+        public SalaryDetail(BasicSalary basicSalary, Allowance allowance, Transportation transportation)
+        {
+            BasicSalary = Validate.ArgumentNotNull(basicSalary, nameof(basicSalary));
+            Allowance = Validate.ArgumentNotNull(allowance, nameof(allowance));
+            Transportation = Validate.ArgumentNotNull(transportation, nameof(transportation));
+        }
+    }
+}
