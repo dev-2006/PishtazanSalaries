14904a53ea28bad9c398d0a681c6d26f35fd75a7
m <m@m.com>
2023-08-25 07:53:38 +0330

    Tests for different salary objects with same value equality added
    Salary is value object so diffrent salary objects with same values are equal

--- Pishtazan.Salaries.Domain.Tests.Unit/Employees/Salaries/SalaryTests.cs
+++ Pishtazan.Salaries.Domain.Tests.Unit/Employees/Salaries/SalaryTests.cs
@@ -31,5 +31,16 @@ namespace Pishtazan.Salaries.Domain.Tests.Unit.Employees.Salaries
 
             Assert.Equal(amount, s.Value);
         }
+
+        [Fact]
+        public void DiffrentNameObjectWithSameValueAreEqual()
+        {
+            var salary1 = new Salary(20);
+            var salary2 = new Salary(20);
+
+            Assert.Equal(salary1, salary2);
+            Assert.True(salary1 == salary2);
+            Assert.False(ReferenceEquals(salary1, salary2));
+        }
     }
 }
