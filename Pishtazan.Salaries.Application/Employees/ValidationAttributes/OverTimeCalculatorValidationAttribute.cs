733d64e019ea642c37b9978d12c8d24bb8df8df4
m <m@m.com>
2023-08-26 02:11:34 +0330

    Support for checking OvertimePolicyCalculators set in ValidationAttribute added

--- Pishtazan.Salaries.Application/Employees/ValidationAttributes/OverTimeCalculatorValidationAttribute.cs
+++ Pishtazan.Salaries.Application/Employees/ValidationAttributes/OverTimeCalculatorValidationAttribute.cs
@@ -1,14 +1,20 @@
-﻿using System.ComponentModel.DataAnnotations;
+﻿using Pishtazan.Salaries.OvertimePolicies.Calculators;
+using System.ComponentModel.DataAnnotations;
 
 namespace Pishtazan.Salaries.Application.Employees.ValidationAttributes
 {
     public class OverTimeCalculatorValidationAttribute : ValidationAttribute
     {
+        public static IOvertimePolicyCalculator[] OvertimePolicyCalculators { get; set; } = null!;
+
         public override bool IsValid(object? value)
         {
             //بررسی وجود یا عدم وجود باید از طریق اتربیوت مربوط به خود بررسی شود و این ولیدیشن این مسئله را بررسی نمی کند
             if (value == null)
                 return true;
+
+            if (OvertimePolicyCalculators == null)
+                throw new InvalidOperationException();
 
             throw new NotImplementedException();
         }
