711fdbdaca84194151c1bb247d71642caeee55a4
m <m@m.com>
2023-08-25 10:12:45 +0330

    Remaining OverTimePolicies implemented

---
+++ Pishtazan.Salaries.OvetimePolicies/Calculators/CalcurlatorC.cs
@@ -0,0 +1,21 @@
+﻿using Pishtazan.Salaries.Domain.Employees.Salaries;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using static Pishtazan.Salaries.Infrastructure.Validation.Validate;
+
+namespace Pishtazan.Salaries.OvetimePolicies.Calculators
+{
+    public class CalcurlatorC
+    {
+        public Salary Calculate(BasicSalary basicSalary, Allowance allowance)
+        {
+            ArgumentNotNull(basicSalary, nameof(basicSalary));
+            ArgumentNotNull(allowance, nameof(allowance));
+
+            return basicSalary + allowance + allowance + allowance;
+        }
+    }
+}
