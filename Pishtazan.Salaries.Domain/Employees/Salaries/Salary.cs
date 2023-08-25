687347bf21c37e9a01aaa91e7315c1a6afa81773
m <m@m.com>
2023-08-25 07:29:01 +0330

    Too few salary amount validation added

--- Pishtazan.Salaries.Domain/Employees/Salaries/Salary.cs
+++ Pishtazan.Salaries.Domain/Employees/Salaries/Salary.cs
@@ -8,9 +8,12 @@ namespace Pishtazan.Salaries.Domain.Employees.Salaries
 {
     public record Salary
     {
+        public const long MIN = 1;
+
         public Salary(long value)
         {
-            
+            if(value < MIN)
+                throw new ArgumentOutOfRangeException(paramName: "salary", message: "salary amount is too few");
         }
     }
 }
