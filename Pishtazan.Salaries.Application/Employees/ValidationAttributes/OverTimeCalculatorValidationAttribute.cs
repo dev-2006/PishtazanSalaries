c58a4c19225e3017695be0107db77447ec6a0f98
m <m@m.com>
2023-08-26 01:56:17 +0330

    Null argument check added to OverTimeCalculatorValidationAttribute

--- Pishtazan.Salaries.Application/Employees/ValidationAttributes/OverTimeCalculatorValidationAttribute.cs
+++ Pishtazan.Salaries.Application/Employees/ValidationAttributes/OverTimeCalculatorValidationAttribute.cs
@@ -6,6 +6,10 @@ namespace Pishtazan.Salaries.Application.Employees.ValidationAttributes
     {
         public override bool IsValid(object? value)
         {
+            //بررسی وجود یا عدم وجود باید از طریق اتربیوت مربوط به خود بررسی شود و این ولیدیشن این مسئله را بررسی نمی کند
+            if (value == null)
+                return true;
+
             throw new NotImplementedException();
         }
     }
