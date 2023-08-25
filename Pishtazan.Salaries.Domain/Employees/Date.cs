0a3773a561697456a7cb6996419251dd223a633b
m <m@m.com>
2023-08-25 12:10:01 +0330

    Valid date str parse implemented

--- Pishtazan.Salaries.Domain/Employees/Date.cs
+++ Pishtazan.Salaries.Domain/Employees/Date.cs
@@ -1,6 +1,7 @@
 ﻿using Pishtazan.Salaries.Domain.Common;
 using System;
 using System.Collections.Generic;
+using System.Globalization;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -18,9 +19,26 @@ namespace Pishtazan.Salaries.Domain.Employees
         /// </example>
         public const int DATE_STRING_LENGTH = 8;
 
-        public int Year { get; }
-        public int Month { get; }
-        public int Day { get; }
+        public const int YEAR_LENGTH = 4;
+        public const int MONTH_LENGTH = 2;
+        public const int DAY_LENGTH = 2;
+
+        public const int YAER_OFFSET = 0;
+        public const int MONTH_OFFSET = YAER_OFFSET + YEAR_LENGTH;
+        public const int DAY_OFFSET = MONTH_OFFSET + MONTH_LENGTH;
+
+        public int Year => getDatePart(pc => pc.GetYear(GregorianDate));
+
+        private int getDatePart(Func<PersianCalendar, int> get) => get(new PersianCalendar());
+
+        public int Month => getDatePart(pc => pc.GetMonth(GregorianDate));
+        public int Day => getDatePart(pc => pc.GetDayOfMonth(GregorianDate));
+        public DateTime GregorianDate { get; private set; }
+
+        internal Date(DateTime gregorianDate)
+        {
+            GregorianDate = gregorianDate;
+        }
 
         public static Date FromString(string value)
         {
@@ -32,7 +50,21 @@ namespace Pishtazan.Salaries.Domain.Employees
             if (anyNonDigitCharsExistsIn(value))
                 throw new ArgumentOutOfRangeException("Date is not valid");
 
-            throw new NotImplementedException();
+            try
+            {
+                int year = int.Parse(value.Substring(YAER_OFFSET, YEAR_LENGTH));
+                int month = int.Parse(value.Substring(MONTH_OFFSET, MONTH_LENGTH));
+                int day = int.Parse(value.Substring(DAY_OFFSET, DAY_LENGTH));
+
+                PersianCalendar persianCalendar = new PersianCalendar();
+                DateTime gregorianDate = persianCalendar.ToDateTime(year, month, day, 0, 0, 0, 0);
+
+                return new Date(gregorianDate);
+            }
+            catch (Exception ex)
+            {
+                throw new ArgumentOutOfRangeException("Date is not valid", ex);
+            }
         }
 
         private static bool stringLengthIsNotCorrect(string value)
