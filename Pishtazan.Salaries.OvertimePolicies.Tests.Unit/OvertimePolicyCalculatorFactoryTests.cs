e17188d4f577e86cb28f76b1dcba47b46f25c135
m <m@m.com>
2023-08-25 10:48:28 +0330

    Ease of OvertimePolicyCalculator get by adding factory and failing test for null chack in Get method

---
+++ Pishtazan.Salaries.OvertimePolicies.Tests.Unit/OvertimePolicyCalculatorFactoryTests.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Pishtazan.Salaries.OvertimePolicies.Tests.Unit
+{
+    public class OvertimePolicyCalculatorFactoryTests
+    {
+        OvertimePolicyCalculatorFactory factory = new OvertimePolicyCalculatorFactory();
+
+        [Fact]
+        public void Get_NullCalculatorName_ThrowsArgumentNullException()
+        {
+            Assert.Throws<ArgumentNullException>(() => factory.Get(null!));
+        }
+    }
+}
---
+++ Pishtazan.Salaries.OvertimePolicies/OvertimePolicyCalculatorFactory.cs
@@ -0,0 +1,14 @@
+﻿using Pishtazan.Salaries.OvertimePolicies.Calculators;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Pishtazan.Salaries.OvertimePolicies
+{
+    public class OvertimePolicyCalculatorFactory
+    {
+        public IOvertimePolicyCalculator Get(string calculatorName) => throw new NotImplementedException();
+    }
+}
