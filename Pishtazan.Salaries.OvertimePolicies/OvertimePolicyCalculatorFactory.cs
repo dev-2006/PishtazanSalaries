3452e286dfa44d7ef4a0353cc9aeb78a46935c9b
m <m@m.com>
2023-08-25 10:52:53 +0330

    Null check implemented in OvertimePolicyCalculatorFactory.Get

--- Pishtazan.Salaries.OvertimePolicies/OvertimePolicyCalculatorFactory.cs
+++ Pishtazan.Salaries.OvertimePolicies/OvertimePolicyCalculatorFactory.cs
@@ -1,14 +1,27 @@
-﻿using Pishtazan.Salaries.OvertimePolicies.Calculators;
+﻿using Pishtazan.Salaries.Infrastructure.Reflection;
+using Pishtazan.Salaries.OvertimePolicies.Calculators;
 using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
-
+using static Pishtazan.Salaries.Infrastructure.Validation.Validate;
 namespace Pishtazan.Salaries.OvertimePolicies
 {
     public class OvertimePolicyCalculatorFactory
     {
-        public IOvertimePolicyCalculator Get(string calculatorName) => throw new NotImplementedException();
+        public IOvertimePolicyCalculator[] OvertimePolicyCalculators { get; }
+
+        public OvertimePolicyCalculatorFactory()
+        {
+            OvertimePolicyCalculators = AssemblyUtil.LoadInstances<IOvertimePolicyCalculator>();
+        }
+
+        public IOvertimePolicyCalculator Get(string calculatorName)
+        {
+            ArgumentNotNull(calculatorName, nameof(calculatorName));
+
+            throw new NotImplementedException();
+        }
     }
 }
