6fe47318062be1356b897047eaff26e1ca56197d
m <m@m.com>
2023-08-25 09:02:37 +0330

    Add failing test for CalculatorA null checking for basic salary argument

---
+++ Pishtazan.Salaries.OvetimePolicies.Tests.Unit/Calculators/CalcurlatorATests.cs
@@ -0,0 +1,19 @@
+﻿using Pishtazan.Salaries.Domain.Employees.Salaries;
+using Pishtazan.Salaries.OvetimePolicies.Calculators;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Pishtazan.Salaries.OvetimePolicies.Tests.Unit.Calculators
+{
+    public class CalcurlatorATests
+    {
+        [Fact]
+        public void Calculate_NullBasicSalary_ThrowsArgumentNullExcpetion()
+        {
+            Assert.Throws<ArgumentNullException>(() => new CalcurlatorA().Calculate(null!, new Allowance(1)));
+        }
+    }
+}
---
+++ Pishtazan.Salaries.OvetimePolicies/Calculators/CalcurlatorA.cs
@@ -0,0 +1,14 @@
+﻿using Pishtazan.Salaries.Domain.Employees.Salaries;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Pishtazan.Salaries.OvetimePolicies.Calculators
+{
+    public class CalcurlatorA
+    {
+        public Salary Calculate(BasicSalary basicSalary, Allowance allowance) => throw new NotImplementedException();
+    }
+}
