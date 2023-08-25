40115cfacd3d8812f148af2bf9b768aa76ffd21e
m <m@m.com>
2023-08-25 10:42:52 +0330

    Support for loading instances of specific interface or type dynamically

--- Pishtazan.Salaries.Infrastructure/Reflection/AssemblyUtil.cs
+++ Pishtazan.Salaries.Infrastructure/Reflection/AssemblyUtil.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Reflection;
 using System.Text;
 using System.Threading.Tasks;
 
@@ -10,7 +11,24 @@ namespace Pishtazan.Salaries.Infrastructure.Reflection
     {
         public static T[] LoadInstances<T>()
         {
-            throw new NotImplementedException();
+            return LoadInstances<T>(Assembly.GetAssembly(typeof(T))!);
+        }
+
+        public static T[] LoadInstances<T>(Assembly assembly)
+        {
+            var types = LoadTypes<T>(assembly);
+
+            return types.Select(t => (T)Activator.CreateInstance(t)!).ToArray();
+        }
+
+        public static Type[] LoadTypes<T>()
+        {
+            return LoadTypes<T>(Assembly.GetAssembly(typeof(T))!);
+        }
+
+        public static Type[] LoadTypes<T>(Assembly assembly)
+        {
+            return assembly.GetTypes().Where(t => t.IsAssignableTo(typeof(T)) && t.IsClass).ToArray();
         }
     }
 }
