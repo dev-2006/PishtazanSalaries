bd7ac55fb15bd6c6e41ead2f6f5fe2e2941642dc
m <m@m.com>
2023-08-26 01:18:59 +0330

    Employee Repository interface added

---
+++ Pishtazan.Salaries.Application/Employees/Repository/IEmployeeRepository.cs
@@ -0,0 +1,18 @@
+﻿using Pishtazan.Salaries.Domain.Employees;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Pishtazan.Salaries.Application.Employees.Repository
+{
+    public interface IEmployeeRepository
+    {
+        Task<Employee?> Load(FullName fullName);
+
+        Task Add(Employee employee);
+
+        Task SaveChanges();
+    }
+}
