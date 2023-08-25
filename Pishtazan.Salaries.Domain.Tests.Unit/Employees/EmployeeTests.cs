e0afd3c4927a5b89e0751d4b46bb2240e1dada09
m <m@m.com>
2023-08-25 14:58:40 +0330

    Employee added with constructor tests

---
+++ Pishtazan.Salaries.Domain.Tests.Unit/Employees/EmployeeTests.cs
@@ -0,0 +1,26 @@
+﻿using Pishtazan.Salaries.Domain.Common.Salaries;
+using Pishtazan.Salaries.Domain.Employees;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Pishtazan.Salaries.Domain.Tests.Unit.Employees
+{
+    public class EmployeeTests
+    {
+        [Fact]
+        public void Constructor_NullFullName_ThrowsArgumentNullException()
+        {
+            Assert.Throws<ArgumentNullException>(() => new Employee(fullName: null!, incomes: new List<IncomeDetail>()));
+        }
+
+        [Fact]
+        public void Constructor_NullIncomes_ThrowsArgumentNullException()
+        {
+            Assert.Throws<ArgumentNullException>(() => new Employee(
+                fullName: new FullName(new FirstName("ali"), new LastName("ahmadi")), incomes: null!));
+        }
+    }
+}
---
+++ Pishtazan.Salaries.Domain/Employees/Employee.cs
@@ -0,0 +1,33 @@
+﻿using Pishtazan.Salaries.Domain.Common.Salaries;
+using Pishtazan.Salaries.Domain.IncomeCalculationStrategies;
+using Pishtazan.Salaries.Infrastructure.Validation;
+using Pishtazan.Salaries.OvertimePolicies.Calculators;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using static Pishtazan.Salaries.Infrastructure.Validation.Validate;
+
+namespace Pishtazan.Salaries.Domain.Employees
+{
+    public class Employee
+    {
+        private ICollection<IncomeDetail> _incomes;
+        public IReadOnlyCollection<IncomeDetail> Incomes => _incomes.ToList();
+
+        public FullName FullName { get; private set; }
+
+        public Employee(FullName fullName)
+        {
+            FullName = ArgumentNotNull(fullName, nameof(fullName));
+            _incomes = new HashSet<IncomeDetail>();
+        }
+
+        public Employee(FullName fullName, ICollection<IncomeDetail> incomes)
+        {           
+            FullName = ArgumentNotNull(fullName, nameof(fullName));
+            _incomes = ArgumentNotNull(incomes, nameof(incomes));
+        }
+    }
+}
