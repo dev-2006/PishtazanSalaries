5ebe8f1a727f1ecb31b737000af6dd63f2f4ccde
m <m@m.com>
2023-08-26 09:05:11 +0330

    test for create salary when employee exists and new salary date is in same month added

--- Pishtazan.Salaries.Application.Tests.Unit/Employees/EmployeeApplicationServiceTests.cs
+++ Pishtazan.Salaries.Application.Tests.Unit/Employees/EmployeeApplicationServiceTests.cs
@@ -3,6 +3,7 @@ using Pishtazan.Salaries.Application.Employees.Contracts.Command;
 using Pishtazan.Salaries.Application.Employees.ValidationAttributes;
 using Pishtazan.Salaries.Domain.Common.Salaries;
 using Pishtazan.Salaries.Domain.Employees;
+using Pishtazan.Salaries.Domain.Employees.Exceptions;
 using Pishtazan.Salaries.Domain.IncomeCalculationStrategies;
 using Pishtazan.Salaries.OvertimePolicies.Calculators.Factory;
 using System;
@@ -56,6 +57,24 @@ namespace Pishtazan.Salaries.Application.Tests.Unit.Employees
 
             Assert.Single(repository.Employees);
             Assert.Equal(2, repository.Employees[0].Incomes.Count);
+        }
+
+        [Fact]
+        public async void HandleCreateSalaryCommand_WhenEmployeeExistsAndNewSalaryDateIsInSameMonth_ThrowsDuplicateSalariesInSameMonthException()
+        {
+            EmployeeRepositoryInMemory repository = EmployeeRepositoryBuilder.CreateRepositoryWithOneEmployeeAndOneSalary();
+
+            EmployeeApplicationService service = new EmployeeApplicationService(repository, incomeCalculation, overtimePolicyFactory);
+
+            var e = await Record.ExceptionAsync(() => service.Handle(new CreateEmployeeSalary(new EmployeeSalary
+            {
+                FirstName = EmployeeRepositoryBuilder.EXIST_EMP_FN,
+                LastName = EmployeeRepositoryBuilder.EXIST_EMP_LN,
+                Date = EmployeeRepositoryBuilder.NOT_EXIST_SALARY_DATE_SAME_MONTH_STR,
+                BasicSalary = 1, Allowance = 2, Transportation = 3, OverTimeCalculator = OVER_TIME_CALCULATOR
+            })));
+
+            Assert.IsType<DuplicateSalariesInSameMonthException>(e);
         }
     }
 }
