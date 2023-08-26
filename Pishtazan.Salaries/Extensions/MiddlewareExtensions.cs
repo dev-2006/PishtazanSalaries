d23d41997345590544d0e435019056ffb0d37ec3
m <m@m.com>
2023-08-26 13:52:35 +0330

    Support for custome api exception handling added

---
+++ Pishtazan.Salaries/Extensions/MiddlewareExtensions.cs
@@ -0,0 +1,11 @@
+﻿using Pishtazan.Salaries.Infrastructure.Middlewares;
+using Microsoft.AspNetCore.Builder;
+
+namespace Pishtazan.Salaries.Extensions
+{
+    public static class MiddlewareExtensions
+    {
+        public static IApplicationBuilder UseApiExceptionHandling(this IApplicationBuilder app)
+            => app.UseMiddleware<ApiExceptionHandlingMiddleware>();
+    }
+}
---
+++ Pishtazan.Salaries/Infrastructure/Middlewares/ApiExceptionHandlingMiddleware.cs
@@ -0,0 +1,83 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using Pishtazan.Salaries.Framework;
+using System;
+using System.Collections.Generic;
+using System.Net;
+using System.Text.Json;
+using System.Threading.Tasks;
+
+namespace Pishtazan.Salaries.Infrastructure.Middlewares
+{
+    public class ApiExceptionHandlingMiddleware
+    {
+        private readonly RequestDelegate _next;
+        private readonly ILogger<ApiExceptionHandlingMiddleware> _logger;
+
+        public ApiExceptionHandlingMiddleware(RequestDelegate next, ILogger<ApiExceptionHandlingMiddleware> logger)
+        {
+            _next = next;
+            _logger = logger;
+        }
+
+        public async Task Invoke(HttpContext context)
+        {
+            try
+            {
+                await _next(context);
+            }
+            catch (Exception ex)
+            {
+                await HandleExceptionAsync(context, ex);
+            }
+        }
+
+        private async Task HandleExceptionAsync(HttpContext context, Exception ex)
+        {
+            string result;
+
+            if (ex is NotFoundDomainException nf)
+            {
+                var problemDetails = new ProblemDetails
+                {
+                    Type = "https://tools.ietf.org/html/rfc7231#section-6.5.1",
+                    Title = nf.Message,
+                    Status = (int)HttpStatusCode.NotFound,
+                    Instance = context.Request.Path,
+                };
+                context.Response.StatusCode = (int)HttpStatusCode.NotFound;
+                result = JsonSerializer.Serialize(problemDetails);
+            }
+            else if (ex is DomainException e)
+            {
+                var problemDetails = new ProblemDetails
+                {
+                    Type = "https://tools.ietf.org/html/rfc7231#section-6.5.1",
+                    Title = e.Message,
+                    Status = (int)HttpStatusCode.BadRequest,
+                    Instance = context.Request.Path,
+                };
+                context.Response.StatusCode = (int)HttpStatusCode.BadRequest;
+                result = JsonSerializer.Serialize(problemDetails);
+            }
+            else
+            {
+                _logger.LogError(ex, $"An unhandled exception has occurred, {ex.Message}");
+                var problemDetails = new ProblemDetails
+                {
+                    Type = "https://tools.ietf.org/html/rfc7231#section-6.6.1",
+                    Title = "Internal Server Error.",
+                    Status = (int)HttpStatusCode.InternalServerError,
+                    Instance = context.Request.Path,
+                    Detail = "Internal server error occurred!"
+                };
+                context.Response.StatusCode = (int)HttpStatusCode.InternalServerError;
+                result = JsonSerializer.Serialize(problemDetails);
+            }
+
+            context.Response.ContentType = "application/json";
+            await context.Response.WriteAsync(result);
+        }
+    }
+}
