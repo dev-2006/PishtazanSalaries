dc4644f3c1d73deee1073e4a341ecce49f28c637
m <m@m.com>
2023-08-26 17:46:40 +0330

    Uniq index on FirstName and LastName columns added

---
+++ Pishtazan.Salaries.Persistence/Migrations/20230826141416_AddUniqIndexToFirstAndLastNameColumns.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Pishtazan.Salaries.Persistence.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddUniqIndexToFirstAndLastNameColumns : Migration
+    {
+        private const string FIRST_LAST_NAME_INDEX = "IX_F_L_NAME";
+
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateIndex(name: FIRST_LAST_NAME_INDEX, table: "Employees",
+                columns: new string[] { "FirstName", "LastName" }, unique: true);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropIndex(FIRST_LAST_NAME_INDEX);
+        }
+    }
+}
