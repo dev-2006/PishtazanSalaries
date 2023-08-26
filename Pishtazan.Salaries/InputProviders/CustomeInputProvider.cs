39739d623967d4c98e65502a4798d0aca5b738f1
m <m@m.com>
2023-08-26 22:29:28 +0330

    Failing test for custom data convert added

---
+++ Pishtazan.Salaries/InputProviders/CustomeInputProvider.cs
@@ -0,0 +1,17 @@
+﻿using Pishtazan.Salaries.Application.Employees.Contracts.Command;
+
+namespace Pishtazan.Salaries.InputProviders
+{
+    public class CustomeInputProvider
+    {
+        public CustomeInputProvider()
+        {
+
+        }
+
+        public EmployeeSalary Convert(string rawData)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
