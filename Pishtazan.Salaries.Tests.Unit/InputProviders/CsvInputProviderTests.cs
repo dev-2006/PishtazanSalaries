535a3e61357671902b6c8500ef385ff9fd2d7385
m <m@m.com>
2023-08-26 22:40:22 +0330

    Failing test for csv data provider added

---
+++ Pishtazan.Salaries.Tests.Unit/InputProviders/CsvInputProviderTests.cs
@@ -0,0 +1,28 @@
+﻿using Pishtazan.Salaries.Application.Employees.Contracts.Command;
+using Pishtazan.Salaries.InputProviders;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Pishtazan.Salaries.Tests.Unit.InputProviders
+{
+    public class CsvInputProviderTests
+    {
+        [Fact]
+        public void Convert_ValidInput_CreatesEmployeeSalaryObject()
+        {
+            CsvInputProvider inputProvider = new CsvInputProvider();
+            EmployeeSalary salary = inputProvider.Convert("Ali,Ahmadi,1200000,400000,350000,14010801");
+
+            Assert.NotNull(salary);
+            Assert.Equal("Ali", salary.FirstName);
+            Assert.Equal("Ahmadi", salary.LastName);
+            Assert.Equal(1200000L, salary.BasicSalary);
+            Assert.Equal(400000L, salary.Allowance);
+            Assert.Equal(350000L, salary.Transportation);
+            Assert.Equal("14010801", salary.Date);
+        }
+    }
+}
---
+++ Pishtazan.Salaries/InputProviders/CsvInputProvider.cs
@@ -0,0 +1,14 @@
+﻿using Pishtazan.Salaries.Application.Employees.Contracts.Command;
+
+namespace Pishtazan.Salaries.InputProviders
+{
+    public class CsvInputProvider
+    {
+        public CsvInputProvider() { }
+
+        public EmployeeSalary Convert(string rawData) 
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
