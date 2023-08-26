0f5ee6ab8501221ade945fcb1a0f682b31b8f8bd
m <m@m.com>
2023-08-26 22:46:09 +0330

    Csv raw data parse implemented

--- Pishtazan.Salaries/InputProviders/CsvInputProvider.cs
+++ Pishtazan.Salaries/InputProviders/CsvInputProvider.cs
@@ -1,4 +1,7 @@
-﻿using Pishtazan.Salaries.Application.Employees.Contracts.Command;
+﻿using Newtonsoft.Json;
+using Pishtazan.Salaries.Application.Employees.Contracts.Command;
+using Pishtazan.Salaries.Domain.Common.Salaries;
+using Pishtazan.Salaries.Domain.Employees;
 
 namespace Pishtazan.Salaries.InputProviders
 {
@@ -8,7 +11,28 @@ namespace Pishtazan.Salaries.InputProviders
 
         public EmployeeSalary Convert(string rawData) 
         {
-            throw new NotImplementedException();
+            Dictionary<string, string> map = createMapOfProperties(rawData);
+            return createObjectFrom(map);
+        }
+
+        private Dictionary<string, string> createMapOfProperties(string rawData)
+        {
+            string[] propertyNames = new string[] { "FirstName", "LastName", "BasicSalary", "Allowance", "Transportation", "Date" };
+            string[] propertyValues = rawData.Split(new string[] { "," }, StringSplitOptions.RemoveEmptyEntries);
+
+            Dictionary<string, string> map = new Dictionary<string, string>();
+
+            for (int i = 0; i < propertyNames.Length && i < propertyValues.Length; i++)
+                map.Add(propertyNames[i], propertyValues[i]);
+
+            return map;
+        }
+
+        private EmployeeSalary createObjectFrom(Dictionary<string, string> map)
+        {
+            string json = JsonConvert.SerializeObject(map);
+
+            return JsonConvert.DeserializeObject<EmployeeSalary>(json)!;
         }
     }
 }
