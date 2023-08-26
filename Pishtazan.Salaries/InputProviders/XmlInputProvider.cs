ed2129f13fe38e8401319d90e7e4026fc5565f0d
m <m@m.com>
2023-08-26 22:56:15 +0330

    Xml input data provider implemented

---
+++ Pishtazan.Salaries/InputProviders/XmlInputProvider.cs
@@ -0,0 +1,16 @@
+﻿using Newtonsoft.Json;
+using Pishtazan.Salaries.Application.Employees.Contracts.Command;
+using System.Text;
+using System.Xml.Serialization;
+
+namespace Pishtazan.Salaries.InputProviders
+{
+    public class XmlInputProvider : IInputProvider
+    {
+        public EmployeeSalary Convert(string rawData)
+        {
+            XmlSerializer serializer = new XmlSerializer(typeof(EmployeeSalary));
+            return (EmployeeSalary)serializer.Deserialize(new MemoryStream(Encoding.UTF8.GetBytes(rawData)))!;
+        }
+    }
+}
