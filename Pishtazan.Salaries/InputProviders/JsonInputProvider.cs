5a27af03c6adb610b2441c3514af7e12a96301c2
m <m@m.com>
2023-08-26 22:52:43 +0330

    json input data provider implemented

---
+++ Pishtazan.Salaries/InputProviders/JsonInputProvider.cs
@@ -0,0 +1,13 @@
+﻿using Newtonsoft.Json;
+using Pishtazan.Salaries.Application.Employees.Contracts.Command;
+
+namespace Pishtazan.Salaries.InputProviders
+{
+    public class JsonInputProvider : IInputProvider
+    {
+        public EmployeeSalary Convert(string rawData)
+        {
+            return JsonConvert.DeserializeObject<EmployeeSalary>(rawData);
+        }
+    }
+}
